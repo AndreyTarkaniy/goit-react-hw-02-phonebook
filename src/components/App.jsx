a498021747ad7ef6d15648f328a9edd5c7534d0f
AndreyTarkaniy <endrubass@gmail.com>
2023-05-09 22:38:07 +0300

    create form component and add formdata to appstate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+// import { nanoid } from 'nanoid';
+import { Container } from 'components/container/container';
+import Form from 'components/form/form';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  formSubmitHandler = dataFromForm => {
+    console.log(dataFromForm.name);
+    console.log(dataFromForm.number);
+
+    // const { name, value } = dataFromForm;
+    const contact = {
+      name: dataFromForm.name,
+      number: dataFromForm.number,
+    };
+
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+  render() {
+    return (
+      <Container>
+        <Form onSubmitFromForm={this.formSubmitHandler} />
+      </Container>
+    );
+  }
+}
---
+++ src/components/form/form.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    // console.log(this.state);
+
+    this.props.onSubmitFromForm(this.state);
+
+    this.reset();
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="">
+          Name
+          <input
+            type="text"
+            name="name"
+            // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+        </label>
+
+        <label htmlFor="">
+          Number
+          <input
+            type="tel"
+            name="number"
+            // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={this.state.number}
+            onChange={this.handleChange}
+          />
+        </label>
+
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+}
+export default Form;
